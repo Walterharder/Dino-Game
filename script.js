1cbc07535c35d894f1688afba2a91eec7f659d53
Walter Mateus Harder Silva <walter.harder@hotmail.com>
2021-02-10 01:33:44 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,8 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32){
+        console.log('Precionou espaço!');
+    }
+}
+document.addEventListener('keyup', handleKeyUp);
