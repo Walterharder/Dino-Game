458a20bc61ade18a09736eccd69dadb332ab715b
Walter Mateus Harder Silva <walter.harder@hotmail.com>
2021-02-11 01:11:28 -0300

    FINALIZADO

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJuping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32){
-        console.log('Precionou espaço!');
+        if(!isJuping){
+            jump();
+        }
     }
 }
+function jump() {
+    isJuping = true;
+    let upInterval = setInterval(() => {
+        if(position >= 150){
+            clearInterval(upInterval);
+            //Descendo
+            let = downInterval = setInterval(() => {
+                if(position <= 0){
+                    clearInterval(downInterval);
+                    isJuping = false;
+                }else{
+                    position -= 20;
+                     dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        }else{
+             //Subindo
+             position +=20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1050 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        }else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            //GameOver
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+        }else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
